64d5348ab988422749a9d72e4ac90b20425d04cf
Antikuz <36120950+Antikuz@users.noreply.github.com>
2020-10-03 00:21:58 +0700

    Change args dest and add few logging string

--- rename.py
+++ rename.py
@@ -5,11 +5,16 @@ from re import match
 import logging
 from pathlib import Path
 
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(format='%(message)s', level=logging.ERROR)
 
 
 def rename_wildcard(pathes, rename_pattern):
+    logging.info(f'\nChange:')
+    root_path = ''
     for path in pathes:
+        if str(path.parent) != root_path:
+            root_path = str(path.parent)
+            logging.info(f'       {root_path}\\')
         newname = rename_pattern.replace('*', path.stem)
         path_new = Path(path.parent).joinpath(newname)
         
@@ -17,60 +22,66 @@ def rename_wildcard(pathes, rename_pattern):
             Path(path).rename(path_new)
         except PermissionError as err:
             logging.error(str(err))
-
-        logging.info(f'Rename: {str(path)} -----> {newname}')
+        
+        logging.info(' '*(len(root_path)+8) + f'{path.name} -----> {newname}')
 
 
 def main(args):
-    logging.info(f'Получены аргументы {args}')
+    logging.info(f'Arguments received: {args}')
 
-    if args.r:
+    if args.recurse:
         try:
-            paths = Path().rglob(args.path)
+            paths = Path().rglob(args.source)
         except OSError as err:
             logging.error(str(err))
     else:
-        paths = Path().glob(args.path)
+        paths = Path().glob(args.source)
+        
     paths = [path for path in paths if path.is_file()]
     
-    if args.v:
+    # If verbose - output found files
+    if args.verbose:
         root_path = ''
+        logging.info('Find:')
         for path in paths:
             if str(path.parent) != root_path:
                 root_path = str(path.parent)
-                logging.info(f'{root_path}:')
-            logging.info(' '*(len(root_path)+1) + path.name)
+                logging.info(f'     {root_path}\\')
+            logging.info(' '*(len(root_path)+6) + path.name)
 
-    # Если в паттерне присутствует * и расширение файла то, меняем расширения
-    if match(r'.*?\*\.\S+', args.pattern):
-        rename_wildcard(paths, args.pattern)
+    # If the pattern contains * and the file ext, then change the ext
+    if match(r'.*?\*.*?\.\S+', args.destination):
+        rename_wildcard(paths, args.destination)
 
+    logging.info(f'\nChanged {len(paths)} files')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description argumentparser')
     parser.add_argument(
-        'path', 
+        'source', 
         action='store', 
-        help='Path to directory with files to rename'
+        help='Path to the directory with files to change'
     )
     parser.add_argument(
-        'pattern', 
+        'destination', 
         action='store', 
-        help='Pattern to rename files or new file name'
+        help='Pattern for changing files'
     )
     parser.add_argument(
         '-r', 
         action='store_true', 
         default=False, 
+        dest='recurse',
         help='Recurse into subdirectories'
     )
     parser.add_argument(
         '-v', 
-        action='store_true', 
+        action='store_true',
+        dest='verbose',
         default=False, 
         help='Verbose'
     )
     args = parser.parse_args()
-    if args.v:
+    if args.verbose:
         logging.getLogger().setLevel(logging.INFO)
     main(args)
