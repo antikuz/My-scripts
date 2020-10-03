840247da6be091f973c4558e632b08f32a68287c
Antikuz <36120950+Antikuz@users.noreply.github.com>
2020-10-03 18:18:32 +0700

    Update rename.py
    Add debug mode. Add convert to relative path. Add funtions get mtime and ctime for possible var in rename files.

--- rename.py
+++ rename.py
@@ -4,30 +4,68 @@ import argparse
 from re import match
 import logging
 from pathlib import Path
+from datetime import datetime as dt
+from os import chdir
 
 logging.basicConfig(format='%(message)s', level=logging.ERROR)
 
+def set_relative_path(args):
+    """Convert absolute path to relative. If the root folder in the current 
+    working directory is different from the root folder in the patch, then 
+    change the current working directory to root folder in the patch. Otherwise 
+    Path.relative_to method will raise ValueError."""
+    try:
+        relative_path = Path(args.source).relative_to(Path.cwd())
+    except ValueError as err:
+        logging.error(str(err))
+        chdir(Path(args.source).root)
+    relative_path = Path(args.source).relative_to(Path.cwd())
+    args.source = str(relative_path)
 
-def rename_wildcard(pathes, rename_pattern):
-    logging.info(f'\nChange:')
+
+def get_ctime(file, format='%y%m%d%H%M%S'):
+    format = '%m%d_%H%M%S'
+    # Get create file time
+    filetime = file.stat().st_ctime
+    datetimetime = dt.fromtimestamp(filetime)
+    ctime = datetimetime.strftime(format)
+    return ctime
+
+
+def get_mtime(file, format='%y%m%d%H%M%S'):
+    format = '%m%d_%H%M%S'
+    # Get modified file time
+    filetime = file.stat().st_mtime
+    datetimetime = dt.fromtimestamp(filetime)
+    mtime = datetimetime.strftime(format)
+    return mtime
+
+
+def rename_wildcard(pathes, args):
+    logging.info(f'\nRename:')
     root_path = ''
     for path in pathes:
         if str(path.parent) != root_path:
             root_path = str(path.parent)
             logging.info(f'       {root_path}\\')
-        newname = rename_pattern.replace('*', path.stem)
+        newname = args.destination.replace('*', path.stem)
         path_new = Path(path.parent).joinpath(newname)
         
-        try:
-            Path(path).rename(path_new)
-        except PermissionError as err:
-            logging.error(str(err))
+        if not args.debug:
+            try:
+                Path(path).rename(path_new)
+            except PermissionError as err:
+                logging.error(str(err))
         
         logging.info(' '*(len(root_path)+8) + f'{path.name} -----> {newname}')
 
 
 def main(args):
     logging.info(f'Arguments received: {args}')
+    
+    # If path absolute glob will raise error, so convert abs path to relative
+    if Path(args.source).anchor:
+        set_relative_path(args)
 
     if args.recurse:
         try:
@@ -51,9 +89,10 @@ def main(args):
 
     # If the pattern contains * and the file ext, then change the ext
     if match(r'.*?\*.*?\.\S+', args.destination):
-        rename_wildcard(paths, args.destination)
+        rename_wildcard(paths, args)
 
-    logging.info(f'\nChanged {len(paths)} files')
+    logging.info(f'\nRenamed {len(paths)} files')
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description argumentparser')
@@ -80,6 +119,13 @@ if __name__ == "__main__":
         dest='verbose',
         default=False, 
         help='Verbose'
+    )
+    parser.add_argument(
+        '-d', 
+        action='store_true',
+        dest='debug',
+        default=False, 
+        help='debug'
     )
     args = parser.parse_args()
     if args.verbose:
